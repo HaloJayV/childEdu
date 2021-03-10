fb484156e2500acf2a770024b295e42b81bb4187
JavaJayV <285075313@qq.com>
2021-03-10 21:57:56 +0800

    重构

---
+++ infoEdu-subject/src/main/java/com/lingwo/infoedu/subject/service/OssService.java
@@ -0,0 +1,23 @@
+package com.lingwo.infoedu.subject.service;
+
+
+import com.lingwo.infoedu.subject.dto.OssCallbackResult;
+import com.lingwo.infoedu.subject.dto.OssPolicyResult;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * oss上传管理Service
+ * Created by macro on 2018/5/17.
+ */
+public interface OssService {
+    /**
+     * oss上传策略生成
+     */
+    OssPolicyResult policy();
+
+    /**
+     * oss上传成功回调
+     */
+    OssCallbackResult callback(HttpServletRequest request);
+}
